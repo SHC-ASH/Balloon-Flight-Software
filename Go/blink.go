fb02cbdb720f1b3896ad68cc4ad172777ac5811c
Ben O'Bryan <bwo0002@uah.edu>
2021-11-10 04:56:32 +0000

    Add working blink program

---
+++ Go/blink.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/stianeikeland/go-rpio"
+)
+
+func main() {
+	fmt.Println("opening gpio")
+	err := rpio.Open()
+	if err != nil {
+		panic(fmt.Sprint("unable to open gpio", err.Error()))
+	}
+
+	defer rpio.Close()
+
+	pin := rpio.Pin(18)
+	pin.Output()
+
+	for x := 0; x < 200; x++ {
+		pin.Toggle()
+		time.Sleep(time.Second / 5)
+	}
+}
